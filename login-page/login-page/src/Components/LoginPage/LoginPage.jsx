a4e248012dd50b369f4842842fd4fe435ae97992
Jiten Kumar <jiten282003@gmail.com>
2024-09-28 00:36:57 +0530

    added the Cascading Style Sheets(CSS) file for the login page

--- login-page/login-page/src/Components/LoginPage/LoginPage.jsx
+++ login-page/login-page/src/Components/LoginPage/LoginPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './LoginPage.css' 
+import { FaUser, FaLock } from "react-icons/fa";
+
 
 const LoginPage = () => {
     return (
@@ -7,10 +9,24 @@ const LoginPage = () => {
             <form action=''>
                 <h1>Login</h1>
                 <div className="input-box">
-                    <input type="text" placeholder='Username' required/>
+                    <input type="text" placeholder='Username' required/> 
+                    <FaUser className='icon'/>
                 </div>
-                <div className="input">
-                    <input type="password" placeholder='Password' required />
+                <div className="input-box">
+                    <input type="password" placeholder='Password' required /> 
+                    <FaLock className='icon'/>
+                </div>
+
+                <div className="remember-forgot">
+                    <label >
+                        <input type="checkbox" />Remember me
+                    </label>
+                    <a href='#'>Forget password</a>
+                </div>
+
+                <button type='submit'>Login</button>
+                <div className="register-link">
+                    <p>Don't have an account? <a href='#'>Register</a></p>
                 </div>
             </form>
         </div>
